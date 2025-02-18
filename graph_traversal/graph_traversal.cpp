98620659e89d227d661f69314cc54b38c0156cdf
fouotsop <patrick.fouotsop@facsiciences-uy1.cm>
2025-02-18 10:32:04 +0100

    added shortestPath algorithm using BFS

--- graph_traversal/graph_traversal.cpp
+++ graph_traversal/graph_traversal.cpp
@@ -17,9 +17,11 @@ void dfs (vector<vector<int>>& graph, vector<bool>& visited, int node) {
     
 }
 
-void bfs (vector<vector<int>>& graph, vector<bool>& visited, int node) {
+void bfs (vector<vector<int>>& graph, int node) {
+
 
     queue<int> queue;
+    vector<bool> visited(graph.size(), false);
     queue.push(node);
     visited[node] = true;
 
@@ -65,6 +67,46 @@ int connected(vector<vector<int>>& graph, int start, int end) {
 
 vector<vector<int>> shortestPath(vector<vector<int>>& graph, int start, int end) {
 
+    vector<bool> visited(graph.size(), false);
+    queue<int> queue;
+    queue.push(start);
+    visited[start] = true;
+
+    vector<vector<int>> parents(graph.size(), vector<int>(graph.size()));
+    parents[start][0] = NULL;
+
+    while (!queue.empty()) {
+
+        int current = queue.front();
+        queue.pop();
+
+        for (int i: graph[current]) {
+
+            if (i = end) {
+                reconstructPath(i,parents);
+            }
+            if (visited[i] == true) continue;
+            visited[i] = true;
+            queue.push(i);
+            parents[i].push_back(current);
+        }
+    }
     
 
 }
+
+    vector<int> reconstructPath(int node, vector<vector<int>>& parents) {
+
+        int i = 0;
+        int parent = parents[node][0];
+        vector<int> path(i);
+        int current = node;
+
+        while (parents[current][0] != NULL) {
+           path.resize(++i);
+           path.insert(path.begin(), current);
+           current = parents[current][0];
+
+        }
+
+    }
